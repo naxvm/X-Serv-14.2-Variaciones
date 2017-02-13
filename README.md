744f124c7add83a8c50761c65e3ed33ec3862b96
Nacho Condés <i.condes@alumnos.urjc.es>
2017-02-13 17:28:50 +0100

    Servidor de imagen realizado

---
+++ README.md
@@ -0,0 +1,1 @@
+La imagen que insertamos tiene que ser de internet, ya que el servidor que hemos programado solo responde con el código de la página, le pidan la página o cualquier recurso de la misma. Por eso, no es capaz de mostrar la imagen local. Si introducimos la URL de una imagen de Internet, sí que puede, ya que el servidor externo sirve la imagen sin ningún problema
---
+++ servidor_imagen.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python3
+
+# Aplicación web que devuelva siempre la misma página HTML,
+# que tendrá que tener al menos una imagen (usando un elemento IMG).
+
+
+import socket
+
+
+
+mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mySocket.bind(('localhost', 1235))
+
+mySocket.listen(5)
+
+
+
+while True:
+    print('Waiting for connections')
+    (recvSocket, address) = mySocket.accept()
+    print('HTTP request received:', recvSocket.recv(1024))
+    recvSocket.send(bytes('HTTP/1.1 200 OK\r\n\r\n' +
+                    '<html><body><h1>Hello World!<img src="http://2.bp.blogspot.com/_63jCiXixFMk/R8xviDmjAII/AAAAAAAAAGQ/vgU_rFcyVO0/S1600-R/gsyc.jpg"></h1></body></html>' +
+                    '\r\n', 'utf-8'))
+    recvSocket.close()
